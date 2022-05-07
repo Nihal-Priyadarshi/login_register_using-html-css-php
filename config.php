fd1771fc6cc2852fa83e399221150681a0fd1758
Nihal Priyadarshi <68635556+Nihal-Priyadarshi@users.noreply.github.com>
2022-05-08 00:44:32 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root'); // username is root for localhost
 define('DB_PASSWORD', ''); // password is blank for localhost
-define('DB_NAME', 'Auditorium');
+define('DB_NAME', 'login');
 
 // Try connecting to the Database
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
